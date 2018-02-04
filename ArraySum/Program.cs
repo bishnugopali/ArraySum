c61c6325166ef4179b0d451648bc3e206a2dcab9
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-04 09:49:41 +0000

    Arraysum complete

--- ArraySum/Program.cs
+++ ArraySum/Program.cs
@@ -10,6 +10,36 @@ namespace ArraySum
     {
         static void Main(string[] args)
         {
+
+            //a program in C# Sharp to find the sum of all elements of the array.
+            int i,n,sum=0;
+            int[] a = new int[100];
+            
+            Console.Write("Input the number of elements to store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} number of elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nThe values store into the array are : \n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0}  ", a[i]);
+            }
+
+            Console.Write("\n\nThe sum of all elements in arrya is :\n");
+            for (i = 0; i < n; i++)
+            {
+                sum += a[i];
+               
+            }
+            Console.Write("{0}  ", sum);
+            Console.ReadLine();
+
         }
     }
 }
